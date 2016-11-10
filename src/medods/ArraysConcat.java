7bac804574cbe1f3c202668d12ba871404f2a040
VitekRomanov <gizinax@yandex.ru>
2016-11-10 17:39:59 +0200

    Refactoring of variables

--- src/medods/ArraysConcat.java
+++ src/medods/ArraysConcat.java
@@ -4,17 +4,17 @@ package medods;
 
 public class ArraysConcat {
 	
-    public static int[] concatArray(int[] arr1, int[] arr2,int k)throws IllegalArgumentException{
+    public static int[] concatArray(int[] arr1, int[] arr2,int startPosition)throws IllegalArgumentException{
     	
     	int arr1Lenght = arr1.length;
     	int arr2Lenght = arr2.length;
     	
-    	if (k>arr1Lenght)  throw new IllegalArgumentException("k mast be < Arrays lenght");
+    	if (startPosition>arr1Lenght)  throw new IllegalArgumentException("startPosition mast be < Arrays lenght");
     		
     	int[] newArr = new int[arr1.length + arr2.length];
-        System.arraycopy(arr1, 0,newArr, 0, k);
-        System.arraycopy(arr2, 0, newArr,k,arr2Lenght);
-        System.arraycopy(arr1,k,newArr,k+arr2Lenght,arr1Lenght-k);
+        System.arraycopy(arr1, 0,newArr, 0, startPosition);
+        System.arraycopy(arr2, 0, newArr,startPosition,arr2Lenght);
+        System.arraycopy(arr1,startPosition,newArr,startPosition+arr2Lenght,arr1Lenght-startPosition);
 		return newArr;
 	}
 	public static void main(String args[]){
--- src/medods/SummFisrtAndLast.java
+++ src/medods/SummFisrtAndLast.java
@@ -5,10 +5,10 @@ public class SummFisrtAndLast {
 	public static boolean summOfPart (int a) throws IllegalArgumentException {
 		 
 		if (a > 9999 || a < 1000)
-			throw new IllegalArgumentException("The namber mast have 4chars");
-		int twoFirstNaber = a/100;
-		int twoLastNaber = a%100;
-		return (twoFirstNaber/10)+(twoFirstNaber%10)==(twoLastNaber/10)+(twoLastNaber%10);
+			throw new IllegalArgumentException("The number mast have 4chars");
+		int twoFirstNubers = a/100;
+		int twoLastNubers = a%100;
+		return (twoFirstNubers/10)+(twoFirstNubers%10)==(twoLastNubers/10)+(twoLastNubers%10);
 		
 	}
 		
