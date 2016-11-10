720c69a00afa5da35af7479b03a4cfedbb67fb7a
VitekRomanov <gizinax@yandex.ru>
2016-11-10 17:34:12 +0200

    correct mistake on class SummFirstAndLast

--- src/medods/SummFisrtAndLast.java
+++ src/medods/SummFisrtAndLast.java
@@ -6,13 +6,14 @@ public class SummFisrtAndLast {
 		 
 		if (a > 9999 || a < 1000)
 			throw new IllegalArgumentException("The namber mast have 4chars");
-		
-		return a%100==a/100;
+		int twoFirstNaber = a/100;
+		int twoLastNaber = a%100;
+		return (twoFirstNaber/10)+(twoFirstNaber%10)==(twoLastNaber/10)+(twoLastNaber%10);
 		
 	}
 		
 	public static void main(String args[]){
-		System.out.println(SummFisrtAndLast.summOfPart(4446));
+		System.out.println(SummFisrtAndLast.summOfPart(4646));
 		System.out.println(1011%100);
 	}
 	
